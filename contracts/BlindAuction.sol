09955d9b26c5c80b97777ed3798a893387325313
unknown <ngkc1996@gmail.com>
2020-10-18 22:21:00 +0800

    initial commit

---
+++ contracts/BlindAuction.sol
@@ -0,0 +1,147 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.5.16;
+
+contract BlindAuction {
+    
+    //static variables
+    address public owner;
+    uint public biddingEnd;
+    uint public revealEnd;
+    uint public biddingDuration = 10; //blocks
+    uint public revealDuration = 10; //blocks
+    bool public ended;
+    
+
+    //state
+    address public highestBidder;
+    uint public highestBid;
+    
+    //events
+    event WinnerClaimed(address winner, uint highestBid);
+
+    //modifiers
+    modifier onlyBefore(uint _block) { require(block.number < _block); _; }
+    modifier onlyAfter(uint _block) { require(block.number > _block); _; }
+
+
+    struct Bid {
+        bytes32 blindedBid; //hash(value, fake, secret)
+        uint deposit;
+    }
+
+    mapping (address => Bid[]) public bids;
+    mapping (address => uint) pendingReturns;
+
+    constructor(
+        
+    ) public {
+        owner = msg.sender;
+        biddingEnd = block.number + biddingDuration;
+        revealEnd = biddingEnd + revealDuration;
+    }
+
+    function bid(bytes32 _blindedBid) 
+        public
+        payable
+        onlyBefore(biddingEnd)
+    {
+        bids[msg.sender].push(Bid({
+            blindedBid: _blindedBid,
+            deposit: msg.value
+        }));
+    }
+
+    // Reveal blinded bids
+    // Refund all except if the bid is currently the highest (potential winner)
+    // The submitted values, fakes, secrets must be in the order of the bids
+    function reveal (
+        uint[] memory _values,
+        bool[] memory _fake,
+        bytes32[] memory _secret
+    )
+        public
+        onlyAfter(biddingEnd)
+        onlyBefore(revealEnd)
+        returns (uint refundAmount)
+    {
+        uint length = bids[msg.sender].length;
+        require(_values.length == length);
+        require(_fake.length == length);
+        require(_secret.length == length);
+
+        uint refund = 0;
+        // iterate through all the bids
+        for (uint i = 0; i < length; i++) {
+            Bid storage bidToCheck = bids[msg.sender][i];
+            (uint value, bool fake, bytes32 secret) =
+                    (_values[i], _fake[i], _secret[i]);
+            if (bidToCheck.blindedBid != keccak256(abi.encodePacked(value, fake, secret))) {
+                // Bid was not actually revealed.
+                // Do not refund deposit.
+                // Check the next bid
+                continue;
+            }
+            refund += bidToCheck.deposit;
+            if (!fake && bidToCheck.deposit >= value) {
+                if (checkIfHighestBid(msg.sender, value))
+                    refund -= value;
+            }
+            // Make it impossible for the sender to re-claim
+            // the same deposit.
+            bidToCheck.blindedBid = bytes32(0);
+        }
+        msg.sender.transfer(refund);
+        return refund;
+    }
+
+    // Withdraw a bid that is the winning bid, 
+    // bit was once the highest bid during the reveal stage
+    // Qn: should this be just added on to the end of checkIfHighestBid fn?
+    function withdraw() public  {
+        uint amount = pendingReturns[msg.sender];
+        if (amount > 0) {
+            // It is important to set this to zero because the recipient
+            // can call this function again as part of the receiving call
+            // before `transfer` returns
+            pendingReturns[msg.sender] = 0;
+            msg.sender.transfer(amount);
+        }
+    }
+
+    /// End the auction and send the highest bid
+    /// to the beneficiary.
+    function claimWinnerReward()
+        public
+        onlyAfter(revealEnd)
+    {
+        //require(!ended);
+        emit WinnerClaimed(highestBidder, highestBid);
+        //ended = true;
+        //beneficiary.transfer(highestBid);
+
+        // register winner address as owner
+    }
+
+    // This is an "internal" function which means that it
+    // can only be called from the contract itself (or from
+    // derived contracts).
+    function checkIfHighestBid(address bidder, uint value) internal
+            returns (bool success)
+    {
+        if (value <= highestBid) {
+            return false;
+        }
+        if (highestBidder != address(0)) {
+            // Refund the previously highest bidder.
+            pendingReturns[highestBidder] += highestBid;
+        }
+        highestBid = value;
+        highestBidder = bidder;
+        return true;
+    }
+
+
+}
+
+
+
---
+++ test/BlindAuction.js
@@ -0,0 +1,85 @@
+const keccak256 = require('keccak256')
+const BlindAuction = artifacts.require("BlindAuction");
+const helper = require('../utils/utils.js');
+
+contract("BlindAuction", async (accounts) => {
+  
+  
+
+  it("should be able to initiate an auction", async() => {
+    let auction = await BlindAuction.deployed();
+    let owner = await auction.owner();
+    assert.equal(owner, accounts[0]);
+  });
+
+  it("should be send a bid", async() => {
+    let auction = await BlindAuction.deployed();
+    await auction.bid(keccak256(4, false, "0x123"), {from: accounts[1], value: web3.utils.toWei("4")})
+    await auction.bid(keccak256(3, false, "0x123"), {from: accounts[2], value: web3.utils.toWei("3")})
+  });
+
+  it("should advance the block", async() => {
+    let block_initial = await web3.eth.getBlockNumber();
+    for (i = 0; i < 10; i++) {
+      await helper.advanceBlock();
+    }
+    let block_final = await web3.eth.getBlockNumber();
+    console.log(block_final);
+    // assert.equal(block_initial + 1, block_final);
+  });
+
+  it("should be able to reveal", async() => {
+    let auction = await BlindAuction.deployed();
+    //await auction.bid(keccak256(4, false, "0x123"), {from: accounts[1], value: web3.utils.toWei("4")})
+    //await auction.bid(keccak256(3, false, "0x123"), {from: accounts[2], value: web3.utils.toWei("3")})
+
+    let amount = await auction.reveal([1], [false], ["0x123"], {from: accounts[2]})
+    console.log(amount)
+    //assert.equal(amount, 3);
+  
+  });
+
+  
+  
+  
+  
+  
+  
+  
+  // // accounts are the list of account created by the Truffle (i.e. 10 key pair)
+  // // by default, the first account will deploy the contract
+  // it("should make deployer the owner", async () => {
+  //   let bank = await Bank.deployed(); // get the deployed Bank contract
+  //   let owner = await bank.owner(); // call the getter on public state variable, https://solidity.readthedocs.io/en/v0.7.1/contracts.html#getter-functions
+  //   assert.equal(owner, accounts[0]); // compare the expected owner with the actual owner
+  // });
+
+  // it("can deposit correctly", async () => {
+  //   let bank = await Bank.deployed();
+  //   // sending 3 Ether to deposit() function from accounts[4],
+  //   // Note that deposit() function in the contract doesn't have any input parameter,
+  //   // but in test, we are allowed to pass one optional special object specifying ethers to send to this
+  //   // contract while we are making this function call.
+  //   // Another similar example here: https://www.trufflesuite.com/docs/truffle/getting-started/interacting-with-your-contracts#making-a-transaction
+  //   let result = await bank.deposit({
+  //     from: accounts[4],
+  //     value: web3.utils.toWei("3"), // all amount are expressed in wei, this is 3 Ether in wei
+  //   });
+
+  //   // get deposited balance
+  //   let deposited = await bank.balance({ from: accounts[4] });
+  //   assert.equal(deposited.toString(), web3.utils.toWei("3"));
+  // });
+
+  // it("can withdraw less than despoited", async () => {
+  //   let bank = await Bank.deployed();
+  //   await bank.deposit({
+  //     from: accounts[0],
+  //     value: web3.utils.toWei("3"),
+  //   });
+  //   await bank.withdraw(web3.utils.toWei("2.9"), { from: accounts[0] });
+
+  //   let deposited = await bank.balance({ from: accounts[0] });
+  //   assert.equal(deposited.toString(), web3.utils.toWei("0.1"));
+  // });
+});
